3e53e60897c5170317c395f973f7f93e94a5ddfa
Jeff Estella <jeffrey.estella@gmail.com>
2021-01-28 10:21:57 -0800

    spread.js: add cast mothers functionality and apply to button

--- spread.js
+++ spread.js
@@ -6,11 +6,13 @@ const range = (start, end) => {
     return output;
 }
 class Row {
-    constructor(html) {
+    constructor(html, isMother) {
         this.active = false;
         this.html = html;
         this.html.textContent = 'O O';
-        this.addListener();
+        if (isMother) {
+            this.addClickListener();
+        }
     }
     toggleState() {
         this.active = !this.active;
@@ -20,7 +22,7 @@ class Row {
             this.html.textContent = 'O O'
         }
     }
-    addListener() {
+    addClickListener() {
         this.html.addEventListener('click', () => {
             this.toggleState();
         })
@@ -28,14 +30,15 @@ class Row {
 }
 
 class Figure {
-    constructor(name, mother=false) { //name = string
+    constructor(name, isMother=false) { //name = string
         this.name = name;
         this.html = this.getFigElement(this.name);
-        this.mother = mother;
-        this.row1 = new Row(this.getRowElement(1));
-        this.row2 = new Row(this.getRowElement(2));
-        this.row3 = new Row(this.getRowElement(3));
-        this.row4 = new Row(this.getRowElement(4));
+        this.isMother = isMother;
+        this.row1 = new Row(this.getRowElement(1), this.isMother);
+        this.row2 = new Row(this.getRowElement(2), this.isMother);
+        this.row3 = new Row(this.getRowElement(3), this.isMother);
+        this.row4 = new Row(this.getRowElement(4), this.isMother);
+        this.rowList = [this.row1, this.row2, this.row3, this.row4]
     }
     getFigElement(name) {
         return document.querySelector(`#${name}`);
@@ -45,23 +48,43 @@ class Figure {
     }
 }
 
-const testFigure = new Figure('f1', true);
-
 class ShieldChart {
     constructor() {
+        this.figureList = [];
+        this.motherList = [];
         for (let i of range(1, 16)) {
             let figName = `f${i}`;
-            let motherCheck = i<5 ? true : false
-            this[figName] = new Figure(figName, motherCheck);
+            let checkMother = i<5 ? true : false;
+            this[figName] = new Figure(figName, checkMother);
+            this.figureList.push(this[figName]);
+            if (checkMother) {
+                this.motherList.push(this[figName]);
+            }
+        }
+        this.activateCastBtn();
+    }
+    castMothers() {
+        for (let fig of this.motherList) {
+            for (let row of fig.rowList) {
+                if (Math.random() > 0.5) {
+                    row.toggleState();
+                }
+            }
         }
     }
-    //Update Chart Listener()
+    activateCastBtn() {
+        let btn = document.querySelector('#cast');
+        btn.addEventListener('click', () => {
+            this.castMothers();
+        })
+    }
+    fillChart() {
+        //Access mothers and construct daughters
+        //make inner function to compare two row states and set daughter row state based on that
+    }
 }
 
-const testShieldChart = new ShieldChart;
+const mainShieldChart = new ShieldChart;
 
 //TO DO
-//1. Move add click listener method to Figure class and apply to rows
-//2. Apply click listener only if figure.mother = true
-//3. Make Cast Mothers method and apply it to button
-//4. Make update chart method to execute upon click events on mother rows
+// ***Make update chart method to execute upon click events on mother rows
